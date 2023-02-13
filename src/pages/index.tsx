e2524c2399c5a31c29e04399d8a03a56e7a196fb
itsdlhqcp <dilhaqued@gmail.com>
2023-02-13 14:07:49 +0530

    listing inside items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,18 +10,47 @@
 //   )
 // }
 
-import { NextPage } from 'next'
-import BaseLayout from '@/styles/components/layouts/BaseLayout'
+// import { NextPage } from 'next'
+// import BaseLayout from '@/styles/components/layouts/BaseLayout'
+
+
+// const Home: NextPage = () => {
+
+//   return (
+//     <BaseLayout>
+//     <div >
+//       Helooooooo
+//     </div>
+//     </BaseLayout>
+//   )
+// }
+// export default Home
+
+/* eslint-disable @next/next/no-img-element */
+
+import type { NextPage } from 'next'
+import { BaseLayout,NftList } from '@/styles/components'
 
 
 const Home: NextPage = () => {
-
   return (
     <BaseLayout>
-    <div >
-      Helooooooo
-    </div>
+      <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+        <div className="absolute inset-0">
+          <div className="bg-white h-1/3 sm:h-2/3" />
+        </div>
+        <div className="relative">
+          <div className="text-center">
+            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Amazing Creatures NFTs</h2>
+            <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+              Mint a NFT to get unlimited ownership forever!
+            </p>
+          </div>
+          <NftList />
+        </div>   
+      </div> 
     </BaseLayout>
   )
 }
+
 export default Home
---
+++ src/styles/components/nft/list/index.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
+import { FunctionComponent } from "react";
+import NftItem from "../item";
+
+
+const NftList: FunctionComponent = () => {
+    return (
+        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+        <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+          <NftItem />
+        </div>
+        <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+          <NftItem />
+        </div>
+        <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+          <NftItem />
+        </div>
+        <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+          <NftItem />
+        </div>
+      </div>
+    
+    )
+}
+
+export default NftList
