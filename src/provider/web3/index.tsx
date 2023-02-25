012837a8b5016eb09c6c14b4646509a61ceef046
itsdlhqcp <dilhaqued@gmail.com>
2023-02-25 11:41:22 +0530

    account abstraction CORRECTED

--- src/provider/web3/index.tsx
+++ src/provider/web3/index.tsx
@@ -2,7 +2,6 @@ import { createContext, FunctionComponent, ReactNode, useContext, useState} from
 import * as React from 'react';
 import { createDefaultState, createWeb3State, loadContract, Web3State } from "./utils";
 import { ethers } from "ethers";
-import { setupHooks } from "@/styles/components/ui/hooks/web3/setupHooks";
 
 
   /// we install npm install ethers@5.6.2 and metamask dependensies to render from here
@@ -50,6 +49,61 @@ export function useWeb3() {
   
   export default Web3Provider;
 
+
+
+
+
+
+
+
+
+// import { createContext, FunctionComponent, ReactNode, useContext, useEffect, useState } from "react"
+// import { createDefaultState, createWeb3State, loadContract, Web3State } from "./utils";
+// import { ethers } from "ethers";
+
+// const Web3Context = createContext<Web3State>(createDefaultState());
+
+
+// type Props = {
+//     children: ReactNode | ReactNode[]
+//   }
+
+// const Web3Provider: FunctionComponent<Props> = ({children}) => {
+//   const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState());
+
+//   useEffect(() => {
+//     async function initWeb3() {
+//       const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+//       const contract =  await loadContract("NftMarket", provider);
+
+//       setWeb3Api(createWeb3State({
+//         ethereum: window.ethereum,
+//         provider,
+//         contract,
+//         isLoading: false
+//       }))
+//     }
+
+//     initWeb3();
+//   }, [])
+
+//   return (
+//     <Web3Context.Provider value={web3Api}>
+//       {children}
+//     </Web3Context.Provider>
+//   )
+// }
+
+// export function useWeb3() {
+//   return useContext(Web3Context);
+// }
+
+// export function useHooks() {
+//   const { hooks } = useWeb3();
+//   return hooks;
+// }
+
+// export default Web3Provider;
 
 
 
