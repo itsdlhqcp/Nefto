680028e5f6bad70e10111e4a6796b0ffa8ab3dbb
itsdlhqcp <dilhaqued@gmail.com>
2023-05-08 23:43:02 +0530

    network change

--- src/styles/components/ui/hooks/web3/useNetwork.ts
+++ src/styles/components/ui/hooks/web3/useNetwork.ts
@@ -3,20 +3,41 @@ import useSWR from "swr";
 import { CryptoHookFactory } from "../../layouts/types/hooks";
 
 
+//Targeted networks are connected with respectiove id here
+const NETWORKS: {[k: string]:string} = {
+    1: "Ethereum Main Network",
+  3: "Ropsten Test Network",
+  4: "Rinkeby Test Network",
+  5: "Goerli Test Network",
+  42: "Kovan Test Network",
+  56: "Binance Smart Chain",
+  1337: "Ganache",
+    
+}
+
+const targetId = process.env.NEXT_PUBLIC_TARGET_CHAIN_ID as string;
+const targetNetwork = NETWORKS[targetId];
+
 type UseNetworkResponse = {
   isLoading: boolean;
+  isSupported: boolean;
+  targetNetwork: string;
 }
 
 type NetworkHookFactory = CryptoHookFactory<string, UseNetworkResponse>
 
-// deps -> provider, ethereum, contract (web3State)
+//here impliment the function to get network id name which connected
 export type useNetworkHook = ReturnType<NetworkHookFactory>
 export const hookFactory: NetworkHookFactory = ({provider, isLoading}) => () => {
   const {data, isValidating, ...swr} = useSWR(
     provider ? "web3/useNetwork" :null,   
     async () => {
-    
-      return "Testing network";  ///if there provider list avail acctnts
+        const chainId = (await provider!.getNetwork()).chainId;
+
+        if (!chainId) {
+          throw "Cannot retreive network. Please, refresh browser or connect to other one."
+        }
+        return NETWORKS[chainId];
      }, {
       revalidateOnFocus: false
      }
@@ -30,8 +51,9 @@ export const hookFactory: NetworkHookFactory = ({provider, isLoading}) => () => 
     ...swr,
     data,
     isValidating,
+    targetNetwork,
+    isSupported: data === targetNetwork,
     isLoading: isLoading || isValidating,
-   
   };
 }
 
--- src/styles/components/ui/navbar/index.tsx
+++ src/styles/components/ui/navbar/index.tsx
@@ -84,103 +84,20 @@ export default function Navbar() {
                         </ActiveLink>
 
 
-
-                      // <a
-                      //   key={item.name}
-                      //   href={item.href}
-                      //   className={classNames(
-                      //     item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      //     'px-3 py-2 rounded-md text-sm font-medium'
-                      //   )}
-                      //   aria-current={item.current ? 'page' : undefined}
-                      // >
-                      //   {item.name}
-                      // </a>
                     ))}
                   </div>
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* <button
-                  type="button"
-                  className="p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="w-6 h-6" aria-hidden="true" />
-                </button> */}
-
-                {/* Profile dropdown */}
-
-{/* 
-                {
-                  false ?
-
-<Menu as="div" className="relative ml-3">
-<div>
-  <Menu.Button className="flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-    <span className="sr-only">Open user menu</span>
-    <img
-      className="w-8 h-8 rounded-full"
-      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-      alt=""
-    />
-  </Menu.Button>
-</div>
-<Transition
-  as={Fragment}
-  enter="transition ease-out duration-100"
-  enterFrom="transform opacity-0 scale-95"
-  enterTo="transform opacity-100 scale-100"
-  leave="transition ease-in duration-75"
-  leaveFrom="transform opacity-100 scale-100"
-  leaveTo="transform opacity-0 scale-95"
->
-  <Menu.Items className="absolute right-0 z-10 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-    <Menu.Item>
-
-      {({ active }) => (
-        <Link
-          href="/profile"
-          className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-        >
-          Your Profile
-        </Link>
-      )}
-    </Menu.Item> */}
-    {/* <Menu.Item>
-      {({ active }) => (
-        <a
-          href="#"
-          className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-        >
-          Settings
-        </a>
-      )}
-    </Menu.Item>
-    <Menu.Item>
-      {({ active }) => (
-        <a
-          href="#"
-          className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-        >
-          Sign out
-        </a>
-      )}
-    </Menu.Item> */}
-  {/* </Menu.Items>
-</Transition>
-</Menu>:
-   <button
-   onClick={() => {
-     // in the next lecture!
-     account.connect();
-   }}
-   type="button"
-   className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
- >
-   Connect Wallet
- </button>
-                } */}
+              <div className="self-center mr-2 text-gray-300">
+                  <span className="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-purple-100 text-purple-800">
+                    <svg className="-ml-0.5 mr-1.5 h-2 w-2 text-indigo-400" fill="currentColor" viewBox="0 0 8 8">
+                      <circle cx={4} cy={4} r={3} />
+                    </svg>
+                    {network.data}
+                  </span>
+                </div>
+             
 
 <Walletbar
                   isInstalled={account.isInstalled}
